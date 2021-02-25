35f353d19f12aeba82fc8b0a7789cce28a643582
Ahmad Zaky W <azakyw@gmail.com>
2021-02-25 18:27:07 +0700

    Initial Commit

---
+++ GameCatalogue/GameCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCatalogueApp.swift
+//  GameCatalogue
+//
+//  Created by Ahmad Zaky on 25/02/21.
+//
+
+import SwiftUI
+
+@main
+struct GameCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
