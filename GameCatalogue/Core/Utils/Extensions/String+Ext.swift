a33f74742c3d1eecdde9c6d2c1dcf4f387c9712d
Ahmad Zaky W <azakyw@gmail.com>
2021-03-12 22:32:16 +0700

    add string+ext

--- GameCatalogue/Core/Utils/Extensions/String+Ext.swift
+++ GameCatalogue/Core/Utils/Extensions/String+Ext.swift
@@ -6,19 +6,3 @@
 //
 
 import Foundation
-
-extension String {
-
-    func toDate(withFormat format: String = "yyyy-MM-dd")-> Date?{
-
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone(identifier: "Asia/Jakarta")
-        dateFormatter.locale = Locale(identifier: "id-ID")
-        dateFormatter.calendar = Calendar(identifier: .gregorian)
-        dateFormatter.dateFormat = format
-        let date = dateFormatter.date(from: self)
-
-        return date
-    }
-}
-   
